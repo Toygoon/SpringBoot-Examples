09047dc4af9827a0588b4b2cad7e80196349dbe4
Toygoon <Toygoon>
2022-02-07 16:10:04 +0900

    Ex2-10, FruitProperty class with ConfigurationProperties

---
+++ community/src/main/java/com/community/community/FruitProperty.java
@@ -0,0 +1,19 @@
+package com.community.community;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+/* The more oriented object style mapping rather than @Value */
+// Lombok settings, this auto generates the getter and setter at compiling
+@Data
+// To use @ConfigurationProperties annotation, @Component annotation muse be defined
+@Component
+// Bind to the list as "fruit"
+@ConfigurationProperties("fruit")
+public class FruitProperty {
+    private List<Map> list;
+}
