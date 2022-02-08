ad3836597d0ac99b130d93037ccf5ecefc7b0be8
Toygoon <toygoon@gmail.com>
2022-02-08 16:27:31 +0900

    Ex3-4, Create BookController class

---
+++ havi/src/main/java/com/havi/controller/BookController.java
@@ -0,0 +1,21 @@
+package com.havi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    // HTTP protocol to "/books" with "GET"
+    @GetMapping("/books")
+    public String getBookList(Model model) {
+        // Get bookList key (list) from bookService
+        model.addAttribute("bookList", bookService.getBookList());
+
+        return "book";
+    }
+}
