92a3ba4dc456cfae0a414b87f9ad54569019274a
Toygoon <toygoon@gmail.com>
2022-02-08 16:53:15 +0900

    Ex3-8, Add JPA related annotations to Book.java

--- havi/src/main/java/com/havi/domain/Book.java
+++ havi/src/main/java/com/havi/domain/Book.java
@@ -4,13 +4,23 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
+@Entity
+@Table
 public class Book {
+    // Specify PK
+    @Id
+    @GeneratedValue
     private Integer idx;
+
+    // Specify data records
+    @Column
     private String title;
+    @Column
     private LocalDateTime publishedAt;
 
     @Builder
