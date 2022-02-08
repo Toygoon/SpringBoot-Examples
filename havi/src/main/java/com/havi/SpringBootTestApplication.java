28caf3cd6697573f3aa67aea884c9df373133d66
Toygoon <toygoon@gmail.com>
2022-02-08 16:19:41 +0900

    Ex3-2, Use @SpringBootTest parameter

--- havi/src/main/java/com/havi/SpringBootTestApplication.java
+++ havi/src/main/java/com/havi/SpringBootTestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HaviApplication {
+public class SpringBootTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HaviApplication.class, args);
+        SpringApplication.run(SpringBootTestApplication.class, args);
     }
 
 }
--- havi/src/test/java/com/havi/SpringBootTestApplicationTests.java
+++ havi/src/test/java/com/havi/SpringBootTestApplicationTests.java
@@ -1,13 +1,36 @@
 package com.havi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+// To operate @SpringBootTest properly, @RunWith annotation must be annotated
+@RunWith(SpringRunner.class)
+@SpringBootTest(value = "value=test", properties = {"property.value=propertyTest"},
+        // classes : Specify the class
+        // webEnvironment : Run Mock servlet
+        classes = {SpringBootTestApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootTestApplicationTests {
+
+    // value : Set the property to apply before the tests
+    // This overrides the default property
+    @Value("${value}")
+    private String value;
+
+    // properties : Add the property before the tests
+    // The structure of property is {key=value} like tuples
+    @Value("${property.value}")
+    private String propertyValue;
 
     @Test
     void contextLoads() {
+        assertThat(value, is("test"));
+        assertThat(propertyValue, is("propertyTest"));
     }
 
 }
