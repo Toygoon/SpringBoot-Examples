e761d5d2b68015929168d4fe6f2a56d0f5d4d6d4
Toygoon <Toygoon>
2022-02-11 19:22:03 +0900

    [MISC] Remove unused dependencies

--- SpringBoot-Community-Web/src/main/java/com/web/oauth/CustomOAuth2Provider.java
+++ SpringBoot-Community-Web/src/main/java/com/web/oauth/CustomOAuth2Provider.java
@@ -1,4 +1,4 @@
-package com.web.oauth2;
+package com.web.oauth;
 
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
