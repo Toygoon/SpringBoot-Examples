722001849e249ed3da5b698deafc901f33601787
Toygoon <toygoon@gmail.com>
2022-02-11 16:53:20 +0900

    Ex5-14, Override UserArgumentResolver

--- SpringBoot-Community-Web/src/main/java/com/web/BootWebApplication.java
+++ SpringBoot-Community-Web/src/main/java/com/web/BootWebApplication.java
@@ -5,19 +5,33 @@ import com.web.domain.enums.BoardType;
 import com.web.domain.User;
 import com.web.repository.BoardRepository;
 import com.web.repository.UserRepository;
+import com.web.resolver.UserArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class BootWebApplication {
+public class BootWebApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(BootWebApplication.class, args);
+    }
+
+    @Autowired
+    private UserArgumentResolver userArgumentResolver;
+
+    /* addArgumentResolvers : Override a method to create new annotations */
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(userArgumentResolver);
     }
 
     /* CommandLineRunner is the interface to run some codes after running application
