c1f6b90b4320d60c9a8cdad96cfcc26b5c9736c6
Toygoon <toygoon@gmail.com>
2022-02-11 17:05:44 +0900

    Ex5-17, resolveArgument(); Get User object from session

--- SpringBoot-Community-Web/src/main/java/com/web/resolver/UserArgumentResolver.java
+++ SpringBoot-Community-Web/src/main/java/com/web/resolver/UserArgumentResolver.java
@@ -6,8 +6,12 @@ import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
 
 @Component
 public class UserArgumentResolver implements HandlerMethodArgumentResolver {
@@ -18,8 +22,12 @@ public class UserArgumentResolver implements HandlerMethodArgumentResolver {
                 && parameter.getParameterType().equals(User.class);
     }
 
+    /* Get authenticated User object from session */
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        return null;
+        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
+        User user = (User) session.getAttribute("user");
+
+        return getUser(user, session);
     }
 }
