ec8723f555f9b4acf625d332daa49b38de11c9e9
Toygoon <toygoon@gmail.com>
2022-03-16 21:02:40 +0900

    Rebase with imported sources from @young891221

---
+++ SpringBoot-Community-Rest-Imported/rest-web/src/main/java/com/community/rest/RestWebApplication.java
@@ -0,0 +1,26 @@
+package com.community.rest;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class RestWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestWebApplication.class, args);
+    }
+}
