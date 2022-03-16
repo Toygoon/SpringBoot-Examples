c3081f56fcc05e9bd89485d0faf306cc13ed9c64
Toygoon <toygoon@gmail.com>
2022-03-16 21:54:07 +0900

    Ex6-15, Apply CORS

--- SpringBoot-Community-Rest-Imported/rest-web/src/main/java/com/community/rest/RestWebApplication.java
+++ SpringBoot-Community-Rest-Imported/rest-web/src/main/java/com/community/rest/RestWebApplication.java
@@ -1,26 +1,43 @@
 package com.community.rest;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class RestWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestWebApplication.class, args);
+    }
+
+    @Configuration
+    // @EnableGlobalMethodSecurity : To use @PreAuthorize, and @PostAuthorize
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    // @EnableWebSecurity : Just enables web security
+    @EnableWebSecurity
+    static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            CorsConfiguration configuration = new CorsConfiguration();
+            /* Allow all origin, method, and header */
+            configuration.addAllowedOrigin(CorsConfiguration.ALL);
+            configuration.addAllowedMethod(CorsConfiguration.ALL);
+            configuration.addAllowedHeader(CorsConfiguration.ALL);
+            // Apply set value (from CorsConfiguration which is implemented with CorsConfigurationSource interface) to UrlBasedCorsConfigurationSource
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+
+            http.httpBasic().and().authorizeRequests()
+                    .anyRequest().permitAll()
+                    // Apply settings
+                    .and().cors().configurationSource(source)
+                    .and().csrf().disable();
+        }
     }
 }
